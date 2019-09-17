3c4466ac782504a5d8bf717954113a615c10f7de
Anthony HUYNH <anthonyhuynh@MacBook-Pro-de-admin.local>
2019-09-17 11:30:38 +0200

    exo branchement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,73 +1,112 @@
 package com.company;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int age = 23;
-        float taille = 1.80f;
-        float height = 5.90551f;
-        String firstname = "Anthony";
-        String lastname = "HUYNH";
-        boolean dunno = true;
-
-        System.out.println(age);
-        System.out.println(height + " ou " + taille);
-        System.out.println(lastname);
-        System.out.println(firstname);
-        System.out.println(dunno);
-
-        age = age + 2;
-
-        System.out.println("mon age apres la coding " + age);
-
-        float pastyear = (int) (age / 2);
-
-        System.out.println("mon age quand j'ai eu mon pc " + pastyear);
-
-        float futuryear = (int) (age * 1.5);
-
-        System.out.println("mon age quand il y'aura une colonie sur mars " + futuryear);
-
-        float rest = age % 2;
-
-        System.out.println(rest);
-
-
         int A;
         int B;
 
         A = 23;
         B = 25;
 
-        if (A >= B){
+        if (A >= B) {
             System.out.println("not a dev yet");
+        } else {
+            System.out.println("you're a dev now !!");
         }
 
-        else {
-            System.out.println( "you're a dev now !!");
-        }
+//        float D = 0.2f;
+//        float CD = 0.5f;
+//        String safe = "you can drive";
+//        String molo = "go on easy bruh";
+//        String yolo = " you only live once but prepare yourself to die MTF";
+//        Scanner sc = new Scanner(System.in);
+//        float TA = sc.nextFloat();
+//
+//
+//        if ( TA < D ){
+//           System.out.println(safe);
+//        }
+//        else {
+//            if ( TA > D && TA < CD )
+//            System.out.println(molo);
+//            else {
+//                System.out.println(yolo);
+//            }
+//        }
 
-        float D = 0.2f;
-        float CD = 0.5f;
-        String safe = "you can drive";
-        String molo = "go on easy bruh";
-        String yolo = " you only live once but prepare yourself to die MTF";
-        Scanner sc = new Scanner(System.in);
-    float TA = sc.nextFloat();
+        int MI = 23;
+        int MJ = 18;
 
-
-        if ( TA < D ){
-           System.out.println(safe);
-        }
-        else {
-            if ( TA > D && TA < CD )
-            System.out.println(molo);
+        if (MI >= MJ) {
+            System.out.println("tu es majeure, tu peut boire !!");
+        } else {
+            if (MI < MJ)
+                System.out.println("tu es mineure, tu ne peux pas boire !!");
             else {
-                System.out.println(yolo);
-            } else if (...)
+                System.out.println("erreur!!");
+            }
         }
+
+        //-----------------------------note bac----------------------
+
+        int n = 10;
+
+        String sp = "Sa passe !";
+        String asb = "Assez bien !";
+        String bi = "Bien !";
+        String trb = "Trés bien !";
+        String exl = "Exellent !!";
+        String rt = "Rattrapage :/";
+
+        if (n >= 10 && n < 11) {
+            System.out.println(sp);
+        } else {
+            if (n >= 12 && n < 14) {
+                System.out.println(asb);
+            } else {
+                if (n >= 14 && n < 16) {
+                    System.out.println(bi);
+                } else {
+                    if (n >= 16 && n < 18) {
+                        System.out.println(trb);
+                    } else {
+                        if (n >= 18 && n <= 20) {
+                            System.out.println(exl);
+                        } else {
+                            if (n<=9 && n == 7){
+                                System.out.println(rt);
+                            } else {
+                                if (n>=0 && n<=6){
+                                    System.out.println("C'est raté :(");
+                                } else{
+                                    System.out.println("hey you MTF you cheated !!");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        //---------------------uti modulo x valeur paire ou impaire------------------
+
+        int v=12;
+
+        String p="valeur paire";
+        String ip="valeur impaire";
+
+        if(v % 2 == 0){
+            System.out.println(p);
+        }
+        else{
+            System.out.println(ip);
+        }
+
+        //-------------------------bi
 
     }
 }
